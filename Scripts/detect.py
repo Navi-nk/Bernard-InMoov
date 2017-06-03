dad9f7b4881837daf096400b7d9729c8c30f5537
Navi-nk <naval.kumar99@gmail.com>
2017-06-03 15:49:32 +0800

    master script added

--- Scripts/detect.py
+++ Scripts/detect.py
@@ -1,12 +1,18 @@
 
 #start open cv
 opencv = Runtime.start("cv","OpenCV")
-opencv.addFilter("fr","FaceRecognizer")
+fr=opencv.addFilter("fr","FaceRecognizer")
 opencv.setDisplayFilter("fr")
 opencv.capture()
+fr.train()
+#fr.setModeTrain()
+#fr.setTrainName("li")
+#sleep(5)
+fr.setModeRecognize()
+#fr.train()
 
 #recognize face
 while 1:
-	fr = opencv.getFilter("fr")
+	#fr = opencv.getFilter("fr")
 	print(fr.getLastRecognizedName())
 	sleep(3)
